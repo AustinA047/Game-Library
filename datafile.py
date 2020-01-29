72d72c87f463f3ef1334863df991c89ebe8afad1
Austin Andrews <aandrews@debian07.CIS.COM>
2020-01-29 11:09:28 -0600

    Finished search function and the print function

---
+++ datafile.py
@@ -0,0 +1,12 @@
+#!usr/bin/python3
+#Austin Andrews
+#01/28/2020
+
+import pickle
+
+games = { 1 :['FPS', 'Halo3', 'Bunjee', 'Microsoft', 'Xbox360', '2007', '10', 'either',
+              '30.00', 'yes', '1/5/2008', 'This game blows chunks']}
+datafile = open("game_lib.pickle", "wb")
+pickle.dump(games, datafile)
+datafile.close()
+
--- game-library.py
+++ game-library.py
@@ -3,22 +3,70 @@
 #01/27/2020
 
 import pickle
-
+games = {}
 def add_game():
     print("\nAdd game is running")
+    
 def print_games():
-    print("\nPrint games is running")
+    key_list = games.keys()
+        
+    for key in key_list:
+        print()
+        print("Genre: ", games[key][0])
+        print("Title: ", games[key][1])
+        print("Developer: ", games[key][2])
+        print("Publisher: ", games[key][3])
+        print("System: ", games[key][4])
+        print("Release Date: ", games[key][5])
+        print("Rating: ", games[key][6])
+        print("Single/Multi/Both: ", games[key][7])
+        print("Beat the game?: ", games[key][8])    
+        print("Pruchase Date: ", games[key][9])
+        print("Extra Notes: ", games[key][10])
+        print("----------------------")  
+    
 def search_games():
-    print("\nSearch games is running")
+    found_one = False
+    name = input("  What is the title of the game? ")
+    for key in games.keys():
+        if name == games[key][1]:
+            found_one = True
+            print()
+            print("Genre: ", games[key][0])
+            print("Title: ", games[key][1])
+            print("Developer: ", games[key][2])
+            print("Publisher: ", games[key][3])
+            print("System: ", games[key][4])
+            print("Release Date: ", games[key][5])
+            print("Rating: ", games[key][6])
+            print("Single/Multi/Both: ", games[key][7])
+            print("Beat the game?: ", games[key][8])    
+            print("Pruchase Date: ", games[key][9])
+            print("Extra Notes: ", games[key][10])
+            print("----------------------")              
+            
+    if not found_one:
+        print("*** NO MATCHES FOUND!***\n")    
+    
 def remove_game():
     print("\nRemove game is running")
+    
 def save_data():
-    print("\nSave data is running")
+    datafile = open("game_lib.pickle", "wb")
+    pickle.dump(games, datafile)
+    datafile.close()
+    print("File Saved")
+    
 def quit():
     print("\nQuit is running")
+    
 keep_going = True
 
-if keep_going:
+datafile = open("game_lib.pickle", "rb")
+games = pickle.load(datafile)
+datafile.close()
+
+while keep_going:
     print("""
     
     MAIN MENU
